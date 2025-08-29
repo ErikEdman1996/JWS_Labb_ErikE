932798cb32d6b1f98ee34361229f3fe4104ae142
Erik Edman <61149651+ErikEdman1996@users.noreply.github.com>
2025-08-29 12:48:01 +0200

    Initital commit.

---
+++ src/main/java/org/example/jws_labb_erike/JwsLabbErikEApplication.java
@@ -0,0 +1,13 @@
+package org.example.jws_labb_erike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwsLabbErikEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwsLabbErikEApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jws_labb_erike/JwsLabbErikEApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jws_labb_erike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwsLabbErikEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
